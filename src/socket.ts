e261f61f626fe78551f0d42f60ce4912fcb9eb6b
hitencodes01 <hitenunnao@gmail.com>
2026-04-19 22:34:36 +0530

    fix: socket connection to render

--- src/socket.ts
+++ src/socket.ts
@@ -1,8 +1,14 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://vsgoi-lead-server.onrender.com/", {
+// const socket = io("https://vsgoi-lead-server.onrender.com/", {
+//   withCredentials: true,
+//   transports: ["websocket", "polling"],
+// });
+
+const socket = io("https://vsgoi-lead-server.onrender.com", {
   withCredentials: true,
   transports: ["websocket", "polling"],
+  autoConnect: true,
 });
 
 export default socket;
