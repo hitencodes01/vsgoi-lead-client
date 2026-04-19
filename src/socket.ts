35d91ff6aa1854ceb861908aad7a763fbf100cda
hitencodes01 <hitenunnao@gmail.com>
2026-04-19 22:03:51 +0530

    resolved

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io("https://vsgoi-lead-server.onrender.com/", {
   withCredentials: true,
+  transports: ["websocket", "polling"],
 });
 
 export default socket;
