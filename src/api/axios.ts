b3032d5dc4f7a1126c91e5c47a099c1c33972d4f
hitencodes01 <hitenunnao@gmail.com>
2026-04-19 20:09:34 +0530

    resolved configuration

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL:[ "http://localhost:8000/api" ],
+  baseURL : "https://huggingface.co/spaces/hitencodes01/vsgoi-lead-server",
   withCredentials: true,
 });
 
--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:8000", {
+const socket = io("https://huggingface.co/spaces/hitencodes01/vsgoi-lead-server", {
   withCredentials: true,
 });
 
